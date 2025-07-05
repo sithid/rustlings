d75513b7b51085ca3f277b83511acfe4a3e02a51
sithid <demonicurges05@gmail.com>
2025-07-05 00:30:24 -0400

    current progress: done through 15_traits/traits1

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,10 +4,23 @@
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day > 23 {
+        None
+    } else if hour_of_day < 22{
+        Some(5)
+    } else {
+        Some(0)
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let result = maybe_icecream(15);
+
+    match result {
+        Some(v) => println!("Result: {v}"),
+        None => println!("Zero Remaining")
+    }
 }
 
 #[cfg(test)]
@@ -18,7 +31,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
+        let icecreams = maybe_icecream(12).unwrap();
 
         assert_eq!(icecreams, 5); // Don't change this line.
     }
--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -20,12 +20,18 @@ fn main() {
     let pretend_user_input = "8";
 
     // Don't change this line.
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
 
-    if cost > tokens {
-        println!("You can't afford that many!");
+    if cost.is_err() {
+        println!("You can't afford that many!")
     } else {
-        tokens -= cost;
-        println!("You now have {tokens} tokens.");
+        let cost_value = cost.unwrap();
+        
+        if tokens < cost_value {
+            println!("You can't afford that many!")
+        } else {
+            tokens -= cost_value;
+            println!("You now have {tokens} tokens.")
+        }
     }
 }
